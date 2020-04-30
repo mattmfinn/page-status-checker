ff2b68b3413820214502b12b38575d15a6ab9428
matt <matt@theconstantvariable.com>
2020-04-30 16:44:11 -0700

    Configuration for the MLS Soccer form.
    crawl -Pseed=https://www.mlssoccer.com/,https://www.mlssoccer.com/news -PcrawlDepth=2 -Pcrawlers=300 -PsectionName=Home,News -PformUrl=https://docs.google.com/forms/d/e/1FAIpQLSfPL6Pma3IzmwGM0AxSwJi6zf-dAhF8_8O6A-bdAm3I9YxdlA/formResponse

--- src/main/java/model/Constant.java
+++ src/main/java/model/Constant.java
@@ -2,9 +2,9 @@ package model;
 
 public interface Constant
 {
-    String SECTION_NAME_COLUMN = "";
-    String REFERRING_URL_COLUMN = "";
-    String WEB_URL_COLUMN = "";
-    String STATUS_CODE_COLUMN = "";
-    String STATUS_CODE_DESCRIPTION_COLUMN = "";
+    String SECTION_NAME_COLUMN = "entry.1003752110";
+    String REFERRING_URL_COLUMN = "entry.491255702";
+    String WEB_URL_COLUMN = "entry.1129867652";
+    String STATUS_CODE_COLUMN = "entry.1738101279";
+    String STATUS_CODE_DESCRIPTION_COLUMN = "entry.1380582650";
 }
