85f834140bcd9ed40a5c4207334921d216896b31
Felix Lee <felixlee@Felixs-MacBook-Pro-2.local>
2020-06-11 16:42:20 -0700

    - Added intelliJ and Gradle specific files to gitignore based on recommendations from https://www.toptal.com/developers/gitignore - Modified constants with Spark Google Form values

--- src/main/java/model/Constant.java
+++ src/main/java/model/Constant.java
@@ -2,9 +2,9 @@ package model;
 
 public interface Constant
 {
-    String SECTION_NAME_COLUMN = "entry.1003752110";
-    String REFERRING_URL_COLUMN = "entry.491255702";
-    String WEB_URL_COLUMN = "entry.1129867652";
-    String STATUS_CODE_COLUMN = "entry.1738101279";
-    String STATUS_CODE_DESCRIPTION_COLUMN = "entry.1380582650";
+    String SECTION_NAME_COLUMN = "entry.568962752";
+    String REFERRING_URL_COLUMN = "entry.1290710427";
+    String WEB_URL_COLUMN = "entry.901713482";
+    String STATUS_CODE_COLUMN = "entry.592316544";
+    String STATUS_CODE_DESCRIPTION_COLUMN = "entry.522381030";
 }
